90924eb60674ecbf29ece947723a3746105d84bc
Yash Gupta <somgupta070703@gmail.com>
2026-07-04 20:40:34 +0530

    commit

--- 02-miniProject/src/App.jsx
+++ 02-miniProject/src/App.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import Section1 from './components/Section 1/Section1'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div className=''>
+    <Section1 />
+    </div>
   )
 }
 
---
+++ 02-miniProject/src/components/Section 1/Section1.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from './Navbar'
+
+const Section1 = () => {
+    return (
+        <div>
+            <Navbar />
+        </div>
+    )
+}
+
+export default Section1
