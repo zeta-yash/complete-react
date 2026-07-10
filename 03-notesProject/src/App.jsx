4ed82d7086b4594e930f1258cda89564ef667c38
Yash Gupta <somgupta070703@gmail.com>
2026-07-11 01:11:44 +0530

    form in react

--- 03-notesProject/src/App.jsx
+++ 03-notesProject/src/App.jsx
@@ -2,7 +2,13 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div className='text-center'>
+      <form>
+
+        <input type="text" placeholder='Enter here' className='border-2 border-blue-500 rounded-2xl '/>
+        <button className='bg-blue-400 rounded-2xl ' type="submit">Submit</button>
+      </form>
+    </div>
   )
 }
 
