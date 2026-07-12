4a0d6e7b41eddaa7629d6146a182c2a97ba9209e
Yash Gupta <somgupta070703@gmail.com>
2026-07-13 01:40:26 +0530

    learnt hooks, statemgmt, form handling, two way data binding

--- 03-notesProject/src/App.jsx
+++ 03-notesProject/src/App.jsx
@@ -1,12 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+
 
 const App = () => {
+  const [text, settext] = useState('')
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    console.log("Submitted by",text);
+    settext('');
+  }
+
   return (
     <div className='text-center'>
-      <form>
+      <form onSubmit={(e) => {
+        submitHandler(e)
+      }}>
 
-        <input type="text" placeholder='Enter here' className='border-2 border-blue-500 rounded-2xl '/>
-        <button className='bg-blue-400 rounded-2xl ' type="submit">Submit</button>
+        <input type="text" 
+        value={text}
+        onChange={(e)=>{
+        settext(e.target.value)
+        }}
+        placeholder='Enter here' 
+        className='border-2 border-blue-500 rounded-2xl ' />
+        <button className='bg-blue-400 rounded-2xl' type="submit" >Submit</button>
       </form>
     </div>
   )
