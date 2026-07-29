a04222df475a606af14290d6b06a7f1466745887
Yash Gupta <somgupta070703@gmail.com>
2026-07-30 00:55:41 +0530

    localStorage learnt

---
+++ 04-localStorage/src/App.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+
+
+const App = () => {
+  const user2det = {
+    name : 'Yash',
+    age : 20,
+    city : 'Delhi'
+  }
+
+  localStorage.setItem("user","Yash")
+  localStorage.setItem("pass","root")
+  const user = localStorage.getItem("user")
+  console.log(user);
+  // localStorage.removeItem("user")
+  localStorage.setItem('user2',JSON.stringify(user2det))
+  const getuser = JSON.parse(localStorage.getItem('user2'));
+  console.log(getuser)
+
+  
+  // localStorage.clear()
+  return (
+    <div>User is : {localStorage.getItem('user')} <br />
+    Password is : {localStorage.getItem('pass')}</div>
+  )
+}
+
+export default App
