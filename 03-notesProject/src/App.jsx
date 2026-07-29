9ac51f8d1be61d6db6b44d655e72521222599a8b
Yash Gupta <somgupta070703@gmail.com>
2026-07-30 00:02:25 +0530

    Delete feature enable

--- 03-notesProject/src/App.jsx
+++ 03-notesProject/src/App.jsx
@@ -14,12 +14,19 @@ const App = () => {
     const copyTask = [...task]
     copyTask.push({title,details})
     setTask(copyTask);
-    console.log(copyTask);
     
     
     setTitle('')
     setDetails('')
 
+  }
+
+  const deleteNote=(idx)=>{
+    const copyTask = [...task];
+    copyTask.splice(idx,1)
+    setTask(copyTask)
+    
+    
   }
 
 
@@ -53,10 +60,17 @@ const App = () => {
         <h1 className='font-bold text-2xl'>Recent Notes</h1>
         <div className='flex flex-wrap gap-5 mt-5 h-full overflow-auto'>
           {task.map(function(elem,idx){
-            return <div key={idx} className='h-50 w-40 p-4 text-black bg-white rounded-2xl'>
-              <h3 className='text-l font-bold leading-tight'>{elem.title}</h3>
+            return <div key={idx} className="flex flex-col item-start justify-between  gap-5 h-50 w-40 bg-cover pt-8 pb-4 px-4 text-black bg-[url('https://imgs.search.brave.com/rd9dgHgNJdvQ0k4mr53zul4ZTY_QI5Sh_q31hcF7G1w/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9zdGF0/aWMudmVjdGVlenku/Y29tL3N5c3RlbS9y/ZXNvdXJjZXMvdGh1/bWJuYWlscy8wNDQv/MjQ5LzI3NS9zbWFs/bC9ibGFuay1hbmQt/d2hpdGUtbm90ZWJv/b2std2l0aC1zcGly/YWwtd2l0aG91dC1i/YWNrZ3JvdW5kLXRl/bXBsYXRlLWZvci1t/b2NrdXAtcG5nLnBu/Zw')] bg-center bg-size-[auto_230px] rounded-2xl">
+              <div>
+                <h3 className='text-l font-bold leading-tight'>{elem.title}</h3>
               {/* <hr className='w-8/10 justify-center text-gray-200 ' /> */}
               <p className='mt-1 font-medium text-gray-500'>{elem.details}</p>
+              </div>
+              <button onClick={()=>{
+                deleteNote(idx)
+              }} className='bg-red-500 rounded cursor-pointer w-full active:translate-y-0.5'>
+                Delete
+              </button>
             </div>
           })}
         </div>
