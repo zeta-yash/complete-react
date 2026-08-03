ef4692cc598b3711907a332e8e8577f762a97300
Yash Gupta <somgupta070703@gmail.com>
2026-08-04 00:04:51 +0530

    gallery in making

--- 05-galleryProject/src/App.jsx
+++ 05-galleryProject/src/App.jsx
@@ -1,8 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
+import axios from 'axios'
 
 const App = () => {
+  const [user, setUser] = useState([])
+
+  const getData = async () => {
+    const response = await axios.get('https://picsum.photos/v2/list?page=1&limit=15')
+    console.log(response.data);
+    setUser(response.data)
+  }
+
+  let printUser = "No image available"
+  if (user.length > 0) {
+    printUser = user.map(function (elem,idx) {
+      // return idx
+      return <div className='h-60 w-80 rounded-xl ' >
+        <img src={elem.download_url} alt="" />
+      </div>
+    })
+  }
   return (
-    <div className='bg-red-500'>App</div>
+    <div>
+      <h1>Hello</h1>
+      <button className='bg-green-500 text-2xl m-4 p-6 rounded-2xl active:scale-95'
+        onClick={getData}>
+        Get Data
+      </button>
+      <div className='flex flex-wrap gap-6 '>
+        {printUser}
+      </div>
+    </div>
   )
 }
 
