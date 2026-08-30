5b62078a3eeb56ce16fdd0889efd77ddeb86471d
Yash Gupta <somgupta070703@gmail.com>
2026-08-30 12:36:48 +0530

    context api main

---
+++ 09-contextAPI-adv/components/Nav2.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Nav2 = () => {
+  return (
+    <div className='nav2'>
+        <h2>Home</h2>
+        <h2>About</h2>
+        <h2>Contact</h2>
+        <h2>Services</h2>
+    </div>
+  )
+}
+
+export default Nav2
---
+++ 09-contextAPI-adv/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Nav2 from './Nav2'
+
+const Navbar = () => {
+  return (
+    <div className='nav'>
+        <h2>
+            Website.com
+        </h2>
+            <Nav2/>
+    </div>
+  )
+}
+
+export default Navbar
